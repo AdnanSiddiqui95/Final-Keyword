05fa52fd44dbfb3e62c35a6affd6c442a39e94ff
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-29 19:56:20 +0530

    Add files via upload

---
+++ AA.java
@@ -0,0 +1,9 @@
+// Final class can not be inherited
+//means it can not have child class like Class BB
+public final class AA 
+{
+	public void show()
+	{
+	System.out.println("Hello Adnan");
+	}
+}
